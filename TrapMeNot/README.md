bca810ff4bec833264211ce2db8e53d5ee06363b
EardwulfC <eardwulfcomfy@gmail.com>
2022-12-09 23:32:05 -0500

    Add project files.

---
+++ TrapMeNot/README.md
@@ -0,0 +1,24 @@
+# TrapMeNot
+
+  * Prevents the new Mistlands Traps from attacking players.
+
+## Installation
+
+### Manual
+
+  * Un-zip `TrapMeNot.dll` to your `/Valheim/BepInEx/plugins/` folder.
+
+### Thunderstore (manual)
+
+  2. Go to Settings > Import local mod > Select `TrapMeNot_v1.0.0.zip`.
+  3. Click "OK/Import local mod" on the pop-up for information.
+
+## Instructions
+
+  * Mod instructions.
+
+## Changelog
+
+### 1.0.0
+
+  * Initial release.
---
+++ TrapMeNot/TrapMeNot.cs
@@ -0,0 +1,46 @@
+﻿using BepInEx;
+
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace TrapMeNot
+{
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+    public class TrapMeNot : BaseUnityPlugin
+    {
+        public const string PluginGuid = "Eardwulf.TrapMeNot";
+        public const string PluginName = "TrapMeNot";
+        public const string PluginVersion = "1.0.0";
+
+        Harmony _harmony;
+
+        public void Awake()
+        {
+            _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), harmonyInstanceId: PluginGuid);
+        }
+
+        [HarmonyPatch(typeof(Trap))]
+        static class TrapPatch
+        { 
+            [HarmonyPrefix]
+            [HarmonyPatch(nameof(Trap.OnTriggerEnter))]
+            static void OnTriggerEnterUpdate(ref Trap __instance)
+            {
+                if(__instance.m_triggeredByPlayers)
+                {
+                    if (!Player.m_localPlayer.m_pvp)
+                    {
+                        __instance.m_triggeredByPlayers = false;
+                    }
+                }
+            }
+        }
+
+
+        public void OnDestroy()
+        {
+            _harmony?.UnpatchSelf();
+        }
+    }
+}
